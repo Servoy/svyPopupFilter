0a4db00f6a317180abfdf02d29bd035fc28bcd9d
Paolo <paronne@servoy.com>
2021-04-08 16:48:39 +0300

    SVYX-203 Change text from Before/After to On or Before/On or After

--- svyPopupFilter/svyPopupFilter.js
+++ svyPopupFilter/svyPopupFilter.js
@@ -92,8 +92,8 @@ var LOCALE = {
 		labelLastYear: 'Last year',
 		operator: {
 			EQUALS: 'Specific Dates',
-			GREATER_EQUAL: 'Later than',
-			SMALLER_EQUAL: 'Before than',
+			GREATER_EQUAL: 'On or After',
+			SMALLER_EQUAL: 'On or Before',
 			BETWEEN: 'Between'
 		}
 	},
